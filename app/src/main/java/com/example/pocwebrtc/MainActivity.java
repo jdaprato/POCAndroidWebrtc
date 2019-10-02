3c09773696177fb63200344729a11ea7f890beca
Julie <jdaprato07@gmail.com>
2019-10-02 20:23:03 +0200

    Add webrtc logic

--- app/src/main/java/com/example/pocwebrtc/MainActivity.java
+++ app/src/main/java/com/example/pocwebrtc/MainActivity.java
@@ -26,10 +26,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize PeerConnectionFactory globals.
+        //Tell the WebRTC library to initalize with audio, video, and video acceleration hardware acceleration enabled (context, initAudio,initVideo and videoCodecHwAcceleration)
+        PeerConnectionFactory.initializeAndroidGlobals(this, true, true, true);
+
+        //Options can be: disableEncryption, disableNetworkMonitor, networkIgnoreMask ...
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        PeerConnectionFactory peerConnectionFactory = new PeerConnectionFactory(options);
+
+        //Creation of videocapturer which takes the image/video from the device's camera (first camera found is used)
+        VideoCapturer videoCapturerAndroid = createVideoCapturer();
+        //Create MediaConstraints - Will be useful for specifying video and audio constraints. More on this later!
+        MediaConstraints constraints = new MediaConstraints();
+
+        //Create a VideoSource instance from videoCapturer created
+        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid);
+        // Creation of videoTrack which gives the stream of data from the device's camera (params > unique ID (can be any String), videoSource)
+        VideoTrack localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+
+        //create an AudioSource instance
+        AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
+        AudioTrack localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+
+        //we will start capturing the video from the camera
+        //params are width,height and fps
+        videoCapturerAndroid.startCapture(1000, 1000, 30);
+
+        //create surface renderer, init it and add the renderer to the track
+        SurfaceViewRenderer videoView = (SurfaceViewRenderer) findViewById(R.id.surface_rendeer);
+        videoView.setMirror(true);
+
+        EglBase rootEglBase = EglBase.create();
+        videoView.init(rootEglBase.getEglBaseContext(), null);
+
+        localVideoTrack.addRenderer(new VideoRenderer(videoView));
+    }
+
+    /**
+     * Create videocapturer from device's image or video
+     * @return videoCapturer
+     */
+    private VideoCapturer createVideoCapturer() {
+        VideoCapturer videoCapturer;
+        videoCapturer = createCameraCapturer(new Camera1Enumerator(false));
+        return videoCapturer;
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // Trying to find a front facing camera!
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        // We were not able to find a front cam. Look for other cameras
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
     }
 }
