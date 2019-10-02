5a9e607ef60bbf15c0fc90c9d6776b1067d8a4bf
Julie <jdaprato07@gmail.com>
2019-10-02 16:20:48 +0200

    Add package and imports

--- app/src/main/java/com/example/pocwebrtc/MainActivity.java
+++ app/src/main/java/com/example/pocwebrtc/MainActivity.java
@@ -1,4 +1,25 @@
-package com.example.pocwebrtc;
+package xyz.vivekc.webrtccodelab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.CameraVideoCapturer;
+import org.webrtc.EglBase;
+import org.webrtc.Logging;
+import org.webrtc.MediaConstraints;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoCapturerAndroid;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
